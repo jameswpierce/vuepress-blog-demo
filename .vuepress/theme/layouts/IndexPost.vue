1cb0b9f438ae98635f91f7993899f75cd8846075
James Pierce <jpierce@clickherelabs.com>
2019-10-23 15:45:06 -0500

    init indexpost layout

---
+++ .vuepress/theme/layouts/IndexPost.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul id="posts-list">
+      <li v-for="post in $pagination.pages">
+        <router-link class="post-link" :to="post.path">{{ post.title }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
