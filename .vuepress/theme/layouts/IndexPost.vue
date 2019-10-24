ea8fddcfec7a23874ae5563e0001dfe0c904eb7e
James Pierce <jpierce@clickherelabs.com>
2019-10-24 11:30:11 -0500

    fix for layout bug

--- .vuepress/theme/layouts/IndexPost.vue
+++ .vuepress/theme/layouts/IndexPost.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    <ul id="posts-list">
-      <li v-for="post in $pagination.pages">
-        <router-link class="post-link" :to="post.path">{{ post.title }}</router-link>
-      </li>
-    </ul>
-  </div>
+  <Layout>
+    <template #page-top>
+      <div class="theme-default-content content__default">
+        <ul id="posts-list">
+          <li v-for="post in $pagination.pages">
+            <router-link class="post-link" :to="post.path">{{ post.title }}</router-link>
+          </li>
+        </ul>
+        <div id="pagination">
+          <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
+          <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
+        </div>
+      </div>
+    </template>
+  </Layout>
 </template>
+<script>
+import Layout from "@theme/layouts/Layout.vue";
+
+export default {
+  components: {
+    Layout
+  }
+};
+</script>
